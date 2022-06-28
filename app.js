5c0496f109f0fc54f443f59f22f7823f209f58e4
Anish <anchukalyangad@gmail.com>
2022-06-28 12:22:21 +0200

    .

---
+++ app.js
@@ -0,0 +1,100 @@
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll("#button");
+const computerScore = document.getElementById("computerScore");
+const playerScore = document.getElementById("playerScore");
+const endGame = document.querySelector("#endGame");
+
+
+let playerSelection;
+let computerSelection;
+let result;
+let cScore = 0;
+let pScore = 0;
+
+choiceBtns.forEach(button => button.addEventListener('click',() => {
+
+    playerSelection = button.textContent;
+    computerTurn();
+    playerText.textContent = `player: ${playerSelection}`;
+    computerText.textContent = `computer: ${computerSelection}`;
+    resultText.textContent = checkWinner();
+    computerScore.textContent = `Score: ${cScore}`;
+    playerScore.textContent = `Score: ${pScore}`;
+    endGame.textContent = gameOver();
+
+}));
+
+function computerTurn(){
+
+    let randNum = Math.floor(Math.random() * 3) + 1;
+
+    switch(randNum){
+        case 1:
+            computerSelection = "ROCK";
+            break;
+        case 2:
+            computerSelection = "PAPER";
+            break;
+        case 3:
+            computerSelection = "SCISSORS";
+            break;
+    }
+}
+function checkWinner(){
+    if (playerSelection === computerSelection){
+        return('Draw!')
+    }
+    else if (playerSelection == "ROCK" && computerSelection =="PAPER"){
+        cScore++
+        return('You lose!,Paper beats Rock.')
+    }
+    else if (playerSelection == "ROCK" && computerSelection =="SCISSORS"){
+        pScore++
+        return('You Win!,Rock beats Scissors.')
+    }
+    else if (playerSelection == "PAPER" && computerSelection =="ROCK"){
+        pScore++
+        return('You Win!,Paper beats Rock.')
+    }
+    else if (playerSelection == "PAPER" && computerSelection =="SCISSORS"){
+        cScore++
+        return('You Lose!,Scissors beats Paper.')
+    } 
+    else if (playerSelection == "SCISSORS" && computerSelection =="ROCK"){
+        cScore++
+        return('You Lose!,Rock beats Scissors.')
+    }  
+    else if (playerSelection == "SCISSORS" && computerSelection =="PAPER"){
+        pScore++
+        return('You Win!,Scissors beats Paper.')
+    }
+
+}
+ function gameOver(){
+
+    if(pScore >= 6){
+        alert('GAME OVER! YOU WON THE MATCH')
+        location.reload()
+        
+        
+    }
+    else if( cScore >= 6) {
+        alert('GAME OVER! YOU LOST THE MATCH')
+        location.reload()
+    }
+        
+}
+
+
+    
+
+ 
+
+
+   
+    
+
+    
+
